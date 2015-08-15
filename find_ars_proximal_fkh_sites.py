6f1bdfdd124f30591e3627b9e59329ad1e71355c
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2015-08-15 13:02:59 +0300

    wip

--- find_ars_proximal_fkh_sites.py
+++ find_ars_proximal_fkh_sites.py
@@ -2,7 +2,10 @@ def find_forkhead(chromosome, fkh_consensus_seq):
     chromosome_sequence = fetch_sequence_of(chromosome)
     rev_comp_chromosome_sequence = rev_comp(chromosome_sequence)
     watson_matches = find_pattern(fkh_consensus_seq, chromosome_sequence)
-    crick_matches = find_pattern(fkh_consensus_seq, rev_comp(chromosome_sequence))
+    crick_matches = find_pattern(fkh_consensus_seq, rev_comp_chromosome_sequence)
+    print(len(watson_matches) + len(crick_matches))
+    fkh_near_acs = acs_proximal(watson_matches) + acs_proximal(crick_matches)
+    print(len(fkh_near_acs))
 
 def fetch_sequence_of(chromosome):
     all_chromosomes = intermine_query("Chromosome")
@@ -10,6 +13,7 @@ def fetch_sequence_of(chromosome):
     for row in all_chromosomes.rows():
         if row["primaryIdentifier"] == chromosome:
             chromosome = row["sequence.residues"]
+    return chromosome
 
 def intermine_query(type):
     from intermine.webservice import Service
@@ -29,12 +33,17 @@ def intermine_query(type):
 def rev_comp(sequence):
     from Bio.Seq import Seq
     from Bio.Alphabet import generic_dna
-    print(type(sequence))
-    # seq = Seq(sequence, generic_dna)
-    # return str(seq.reverse_complement())
+    seq = Seq(sequence, generic_dna)
+    return str(seq.reverse_complement())
 
 def find_pattern(pattern, sequence):
     import regex
     return regex.findall(pattern, sequence)
+
+def acs_proximal(sequence_list):
+    import re
+    acs = '(AAC[TA]AAA[CT][GA]TAAA[AT][AT][GAT][GAT]){e<=1}'
+    acs_prox = [ seq for seq in sequence_list if re.search(acs, seq) ]
+    return acs_prox
 
 find_forkhead('chrI', '(.{25}[AG]TAAA[CT]A.{72}[AG]TAAA[CT]A){e<=2}')
